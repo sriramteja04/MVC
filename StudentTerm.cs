e0cc8b776a3f8771f23de7922b2c751e36abf5f6
prashanth thallada <35477198+prashanththallada@users.noreply.github.com>
2019-04-23 15:39:19 -0500

    Add files via upload
    Included the string annotation for StudentTerm.cs and added the navigation property for DegreePlan.cs

---
+++ StudentTerm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC.Models
+{
+    public class StudentTerm
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int StudentTermId { get; set; }
+
+        public string StudentId { get; set; }
+        public int TermId { get; set; }
+
+        [Required]
+        [Display(Name = "Term label")]
+        [StringLength(10, ErrorMessage = "term label cannot be longer than 10 characters.")]
+        public string TermLabel { get; set; }
+
+        public bool Done { get; set; }
+
+        public DegreePlan DegreePlan { get; set; } 
+
+        
+    }
+}
